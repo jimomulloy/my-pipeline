f411d0e251d4eb51ca2b9e92a582ca49eb78f1ee
jimomulloy <jimomulloy@gmail.com>
2023-04-30 12:32:33 +0100

    add stage

--- src/main/java/com/myorg/MyPipelineStack.java
+++ src/main/java/com/myorg/MyPipelineStack.java
@@ -1,12 +1,19 @@
 package com.myorg;
 
 import java.util.Arrays;
+
+import org.jetbrains.annotations.NotNull;
+
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.StageProps;
 import software.amazon.awscdk.pipelines.CodePipeline;
 import software.amazon.awscdk.pipelines.CodePipelineSource;
+import software.amazon.awscdk.pipelines.ManualApprovalStep;
 import software.amazon.awscdk.pipelines.ShellStep;
+import software.amazon.awscdk.pipelines.StageDeployment;
 
 public class MyPipelineStack extends Stack {
     public MyPipelineStack(final Construct scope, final String id) {
@@ -23,5 +30,15 @@ public class MyPipelineStack extends Stack {
                 .commands(Arrays.asList("npm install -g aws-cdk", "cdk synth"))
                 .build())
              .build();
+        
+        @NotNull
+		StageDeployment testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", StageProps.builder()
+                .env(Environment.builder()
+                    .account("942706091699")
+                    .region("eu-west-2")
+                    .build())
+                .build()));
+        
+        testingStage.addPost(new ManualApprovalStep("approval"));
     }
 }
